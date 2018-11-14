cc031f3c4461c04080489225f5b250f80f97e778
Rahul Kumar <rahul10.pu@gmail.com>
2018-11-14 14:39:42 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# GitHUB-Profile-Analyser
+# Github Profile Analyzer Web-Application
+
+* A tool that will help you to analyze your **Github Repositories**.You can check every important detail at one place on same page.The project is using threading to make it fast.
+
+### What will our Web app do (Objectives) ?
+* As the name suggests, it **analyses the User’s GitHub Profile.**
+* After Analysing User’s Github profile, it gives important details about **user’s repositories such as how many forks, clones have been made on each repository.**
+* Apart from that, it also shows how many people *(unique and total)* have visited the user’s repos.
+* There is another feature which **allows the user to have a 2d graph representation of all the details in user’s repos.**
+
+### Why do we need GitHub Profile Analyser ?
+The Github Profile Analyser tells the User about the no. of people visiting on his repos which **helps him/her in recognizing the popularity of his repos.** This helps the user in deciding or setting up the priorities among the repos as the repos that are more popular, obviously requires a much needed attention. The **profile analyser also tells how many forks and clones have been made since the user has made the repo. This helps the user in understanding the need of the project among the developers.**
+
+## Technology Stack
+	HTML,CSS,Bootstrap,JS(Frontend)
+	GitHub API connection.
+	Python,Flask,Jinja2(Backend)
+
+
+### Front-End Information
+
+##### The front-end consists of 3 webpages:
+
+* The first page is **Homepage** where the user needs to **Login using his Github username and password.**
+
+### Home Page | Login Page :-
+
+![LoginPage](static/images/demo.png)
+
+* The Second one is **Stats Page** where the user will be able to see the details **about his repos such as REPO NAME, CLONES, FORKS, UNIQUE VISITORS, TOTAL VISITORS and STARS.** In this page, the **User profile Pic** is also displayed in the left side **with his name, bio, following and followers.**
+
+### Stats Page | Main Page :-
+
+![MainPage](static/images/profile.png)
+
+* The Third page is of **Graph Page** which has been produced using **PLOTLY**. Here a **2-D Graph** is generated which shows which repo is **more popular.**
+
+### Graph Page :-
+
+![GraphPage](static/images/graph.png)
+
+* The Final page is of **Error Page** which displayed in case Server Breaks, Internet Connection User, Unauthenticated Login, **Page Not Found Error**.
+
+### Error-404  Page :-
+
+![ErrorPage](static/images/error.png)
+
+### Instructions:
+
+* The first page is usual login page which is made by using Bootstrap and additional CSS. It has two fields one is for **username** and another one is for **password**. Since **GitHub’s API** has been used, user can login only using **GitHub’s credentials**.
+
+* As soon as user hit the **Submit button**,*entered data is send to GitHub API for verification. If entered data is incorrect, then user will get error message displaying “Error: Bad credentials”* else user will be redirected to the main home page.
+
+* In the home page user can all his/her repositories in the form of a table. Table has many columns showing different information like repository name, total views, unique views, fork, clone etc. All this information is provided by using the GitHub’s API.
+There is another additional feature of graph in this project. User can see information about its repositories in a 2D graph. This graph has been made in the Plotly.
+
+### Workflow
+
+* During login, the user has to enter his Github username and password. If username or password is not valid, then an error message will occur “Error: Bad Credentials”.
+
+* If the user left the username and password textboxes empty and clicked on login, a message will be shown “Please fill out these fields”.
+After logging in, the main web-application opens which is the Github profile Analyser. Here the user can see a table which tells all the important details about all the user’s repositories.
+
+
+* On clicking graph button, the graph page is opened where 2d representation can be seen of all the repos against total visits etc.
+
+* There are different options or tools which have been made available using PLOTLY. These tools include download plot as a PNG file, edit in chart studio, zoom, pan etc.
+
+
+### GitHub API Connection:
+
+* An API is basically a set of functions and procedures that allow the creation of applications which access the features or data of an operating system, application, or other service. To connect with Github API, we registered our web-application with auth0 in developer settings on github.com. Then github will provide us with client-id and client secret which we then copied on auth0 fields. Once we successfully authenticate a user, GitHub includes an Access Token in the user profile it returns to Auth0. This token is used to call their API. Github API is responsible for why we were able to analyse profiles from github user. Below is the code for a user which made this possible:
+
+
+#### Designed With :heart: by  [@rahul10-pu](https://rahul10-pu.github.io/   "My PortFolio Link")
---
+++ my_app.py
@@ -0,0 +1,121 @@
+from flask import Flask, render_template, redirect,url_for, request, session, flash, g, make_response, send_file
+from wtforms import Form
+import requests
+from multiprocessing.pool import ThreadPool
+import os
+
+
+app = Flask(__name__)
+app.secret_key = os.urandom(134)
+
+@app.route('/')
+def login_form():
+    if 'data' not in session:
+        return render_template("index.html")
+    else:
+        return render_template("main.html",data=session['data'],graph_y=session['graph_y'])
+
+
+def resultant_data(user_info,username,password):
+    def fetch_url(url):
+        try:
+            response = requests.get(url,auth=(username,password))
+            return response, None
+        except Exception as e:
+            return  None, e
+    try:
+        user_info_json = user_info.json()
+        repos_request = requests.get('https://api.github.com/users/' +user_info_json['login']+ '/repos', auth=(username,password))
+        repos_json = repos_request.json()
+        repos_list = []
+
+        urls_1 = []
+        urls_2 = []
+        for i in range(len(repos_json)):
+            urls_1.append('https://api.github.com/repos/'+user_info_json['login']+'/'+repos_json[i]['name']+'/traffic/clones')
+            urls_2.append('https://api.github.com/repos/'+user_info_json['login']+'/'+repos_json[i]['name']+'/traffic/views')
+
+        response_clone_list = []
+        response_unique_traffic_list = []
+        response_total_traffic_list = []
+
+        raw_results_1 = ThreadPool(20).map(fetch_url, urls_1)
+        for response_clone, error in raw_results_1:
+            if error is None:
+                response_clone_list.append(response_clone.json()['count'])
+        raw_results_2 = ThreadPool(20).map(fetch_url, urls_2)
+        for response_clone, error in raw_results_2:
+            if error is None:
+                response_unique_traffic_list.append(response_clone.json()['uniques'])
+                response_total_traffic_list.append(response_clone.json()['count'])
+
+        for i in range(len(repos_json)):
+            #repos_list contains name of repo,watchers_count,language used,forks_count,clones_count
+            repos_list.append([repos_json[i]['name'],response_clone_list[i],repos_json[i]['forks_count'],response_unique_traffic_list[i],response_total_traffic_list[i],repos_json[i]['watchers_count']])
+        keys_for_dict = ['avatar','email','followers','following','name','username','bio','url','public_repos','repos_list']
+        values_for_dict = [user_info_json['avatar_url'],user_info_json['email'],user_info_json['followers'],user_info_json['following'],user_info_json['name'],user_info_json['login'],user_info_json['bio'],user_info_json['url'],user_info_json['public_repos'],repos_list]
+        result_dict = dict(zip(keys_for_dict,values_for_dict))
+        for i in result_dict['repos_list']:
+            i.append(i[1]+i[2]+i[3]+i[5])
+        result_dict['repos_list'].sort(key=lambda x:x[-1],reverse=True)
+        for i in range(len(result_dict['repos_list'])):
+            result_dict['repos_list'][i].insert(0,i+1)
+        return result_dict
+    except Exception as e:
+        return str(e)
+
+
+
+
+
+@app.route('/signin/',methods=["GET","POST"])
+def login_check():
+    error = ''
+    try:
+        if 'data' in session:
+            return render_template('main.html',data=session['data'],graph_x=session['graph_x'],graph_y=session['graph_y'])
+        elif request.method == "POST":
+    	    attempted_username = request.form['userid']
+    	    attempted_password = request.form['password']
+    	    check_user = requests.get('https://api.github.com/user', auth=(attempted_username, attempted_password))
+    	    if check_user.status_code == 200:
+    	    	data=resultant_data(check_user,attempted_username, attempted_password)
+    	    	if type(data)==dict:
+                        # graph_x_list = []
+                        graph_y_list = []
+
+                        for i in range(len(data['repos_list'][0:5])):
+                            # graph_x_list.append(str(data['repos_list'][i][1]))
+                            graph_y_list.append(data['repos_list'][i][-1])
+                        session['data'] = data
+                        session['graph_y'] = graph_y_list
+                        if 'data' in session:
+                            return render_template('main.html',data=session['data'],graph_y=session['graph_y'])
+    	    	else:
+                        return render_template('index.html',error=data)
+    	    else:
+    	    	return render_template("index.html",error=check_user.json()['message'])
+        else:
+            return render_template("index.html")
+    except Exception as e:
+        return render_template("error.html")
+
+@app.route('/logout/')
+def logout():
+    error = ''
+    if 'data' in session:
+        session.pop('data',None)
+        session.pop('graph_x',None)
+        session.pop('graph_y',None)
+        return redirect("//")
+    else:
+        return render_template("index.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("error.html")
+
+host="0.0.0.0"
+if __name__ == "__main__":
+    app.run(debug=True,port=9999)
